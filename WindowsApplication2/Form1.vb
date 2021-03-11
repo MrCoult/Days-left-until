863fa49c2d40242c9e8abaed75158d953f0bfb8b
MrCoult <80452894+MrCoult@users.noreply.github.com>
2021-03-11 13:44:10 +0200

    Add files via upload

---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Select Case ComboBox1.Text
+            Case "Zile"
+                TextBox1.Text = "Intre cele doua dati sunt " & Math.Abs(DateDiff(DateInterval.Day, DateTimePicker1.Value, DateTimePicker2.Value)) & " " & ComboBox1.Text
+            Case "Saptamani"
+                TextBox1.Text = "Intre cele doua dati sunt " & Math.Abs(Math.Floor(DateDiff(DateInterval.Day, DateTimePicker1.Value, DateTimePicker2.Value) / 7)) & " " & ComboBox1.Text
+            Case Else
+                MsgBox("Nu ati ales ""zile"" sau ""saptamani""", vbCritical)
+        End Select
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker2.ValueChanged
+
+    End Sub
+End Class
